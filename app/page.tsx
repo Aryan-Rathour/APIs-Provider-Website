8a69ff1ef9a63e84ebd10b94ed0ec565d64472dd
Aryan-Rathour <aryanrathour066@gmail.com>
2025-01-31 12:09:46 +0530

    adding button in 3d flipcard

--- app/page.tsx
+++ app/page.tsx
@@ -16,12 +16,13 @@ export default function Home() {
         {/* Adjusted px-8 */}
         <HeroSection />
         <section className="flex flex-wrap gap-4 md:gap-12 lg:gap-12 mb-16 justify-center">
-          
           <div className="perspective-1000">
             <div className="relative w-96 h-80 transform-style-preserve-3d transition-transform duration-500 hover:rotate-y-180">
               {/* Front side */}
               <div className="absolute  w-full h-full bg-purple-500  flex items-center justify-center backface-hidden rounded-lg border border-gray-200 shadow-md">
-                <h2 className="text-white text-xl font-semibold">Popular API</h2>
+                <h2 className="text-white text-xl font-semibold">
+                  Popular API
+                </h2>
               </div>
 
               {/* Back side */}
@@ -34,6 +35,9 @@ export default function Home() {
                   <li>Weather Forecast</li>
                   <li>Currency Exchange Rates</li>
                 </ul>
+                <button className="px-4 py-2 mt-8 border border-purple-500 text-purple-500 rounded-lg hover:bg-purple-100 transition flex items-center gap-2">
+                  Learn more <span className="p-1">&rarr;</span>
+                </button>
               </div>
             </div>
           </div>
@@ -42,7 +46,9 @@ export default function Home() {
             <div className="relative w-96 h-80 transform-style-preserve-3d transition-transform duration-500 hover:rotate-y-180">
               {/* Front side */}
               <div className="absolute w-full h-full bg-purple-500  flex items-center justify-center backface-hidden rounded-lg border border-gray-200 shadow-md">
-                <h2 className="text-white text-xl font-semibold">Latest Blog Posts</h2>
+                <h2 className="text-white text-xl font-semibold">
+                  Latest Blog Posts
+                </h2>
               </div>
 
               {/* Back side */}
@@ -55,28 +61,32 @@ export default function Home() {
                   <li>Best Practices for API Usage</li>
                   <li>Understanding API Rate Limits</li>
                 </ul>
+                <button className="px-4 py-2 mt-8 border border-purple-500 text-purple-500 rounded-lg hover:bg-purple-100 transition flex items-center gap-2">
+                  Learn more <span className="p-1">&rarr;</span>
+                </button>
               </div>
             </div>
           </div>
 
-          <div className="perspective-1000">
-            <div className="relative w-96 h-80 transform-style-preserve-3d transition-transform duration-500 hover:rotate-y-180">
-              {/* Front side */}
-              <div className="absolute w-full h-full bg-purple-500  flex items-center justify-center backface-hidden rounded-lg border border-gray-200 shadow-md">
-                <h2 className="text-white text-xl font-semibold">Documentation</h2>
-              </div>
+          <div className="relative w-96 h-80 transform-style-preserve-3d transition-transform duration-500 hover:rotate-y-180">
+            <div className="absolute w-full h-full bg-purple-500  flex items-center justify-center backface-hidden rounded-lg border border-gray-200 shadow-md">
+              <h2 className="text-white text-xl font-semibold">
+                Documentation
+              </h2>
+            </div>
 
-              {/* Back side */}
-              <div className="rounded-lg absolute w-full h-full bg-gray-50 flex flex-col items-center justify-center transform rotate-y-180 backface-hidden">
-                <h3 className="text-purple-500 text-xl font-semibold mb-2 p-6">
-                  Learn how to use our APIs
-                </h3>
-                <ul className="list-disc text-center pl-10">
-                  <li>Getting Started Guide</li>
-                  <li>API Reference</li>
-                  <li>Code Examples</li>
-                </ul>
-              </div>
+            <div className="rounded-lg absolute w-full h-full bg-gray-50 flex flex-col items-center justify-center transform rotate-y-180 backface-hidden">
+              <h3 className="text-purple-500 text-xl font-semibold mb-2 p-2">
+                Learn how to use our APIs
+              </h3>
+              <ul className="list-disc text-center pl-10">
+                <li>Getting Started Guide</li>
+                <li>API Reference</li>
+                <li>Code Examples</li>
+              </ul>
+              <button className="px-4 py-2 mt-8 border border-purple-500 text-purple-500 rounded-lg hover:bg-purple-100 transition flex items-center gap-2">
+                Learn more <span className="p-1">&rarr;</span>
+              </button>
             </div>
           </div>
         </section>
